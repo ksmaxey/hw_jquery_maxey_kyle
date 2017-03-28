0b3dab0be3c202716c8950e9e4a8babd9331a892
Kyle Maxey <ksmaxey@gmail.com>
2017-03-28 09:31:59 -0400

    add headings function to master branch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    var headings = $('h2');
+    headings.eq(3).html("I'm Kyle Maxey, and I'm learning jQuery!");
+});
+
+$(document).ready(function () {
     var paragraphs = $('p');
     paragraphs.css({'background-color': '#8a8a8a', 'color': 'white', 'padding': '1em', 'line-height': 1.5});
 });
